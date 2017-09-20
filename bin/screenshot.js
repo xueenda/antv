f9ad0463b20146625a04e06a715bbfc611a5465f
leungwensen <leungwensen@gmail.com>
2017-09-20 10:07:14 +0800

    robust screenshot service

--- bin/screenshot.js
+++ bin/screenshot.js
@@ -2,8 +2,14 @@
 const debug = require('debug')('app:screenshot');
 const MAX_POOL_SIZE = require('os').cpus().length;
 const Nightmare = require('nightmare');
-const DataURI = require('datauri');
+const connect = require('connect');
+const getPort = require('get-port');
+const http = require('http');
+const serveStatic = require('serve-static');
 const program = require('commander');
+const {
+    writeFileSync
+} = require('fs');
 const {
     queue
 } = require('d3-queue');
@@ -45,73 +51,98 @@ debug(screenshots);
 // assets
 mkdir('-p', destAssets);
 
-const DELAY = 6000;
-const q = queue(MAX_POOL_SIZE > 2 ? MAX_POOL_SIZE - 1 : MAX_POOL_SIZE);
+const app = connect();
+app.use('/', serveStatic(dest));
+getPort().then(port => {
+    http.createServer(app).listen(port);
+    const url = `http://127.0.0.1:${port}`;
+    debug(`server is ready on port ${port}! url: ${url}`);
+    const DELAY = 6000;
+    const q = queue(MAX_POOL_SIZE > 2 ? MAX_POOL_SIZE - 1 : MAX_POOL_SIZE);
 
-function renderFile(filename, template) {
-    filename = resolve(src, filename);
-    const templateMap = program.dev ? loadTemplates(resolve(theme.root, theme.templates)) : TEMPLATE_MAP;
-    const config = program.dev ? loadConfig(program.config) : CONFIG;
-    return md2html(filename, src, config, templateMap, template, program.dev);
-}
+// markdown rendering
+    function renderFile(filename, template) {
+        filename = resolve(src, filename);
+        const templateMap = program.dev ? loadTemplates(resolve(theme.root, theme.templates)) : TEMPLATE_MAP;
+        const config = program.dev ? loadConfig(program.config) : CONFIG;
+        return md2html(filename, src, config, templateMap, template, program.dev);
+    }
 
-screenshots.forEach(task => {
-    const demoSrc = join(src, task.src);
-    const screenshotDest = join(dest, task.dest);
-    const template = task.template;
+    screenshots.forEach(task => {
+        const demoSrc = join(src, task.src);
+        const screenshotDest = join(dest, task.dest);
+        const template = task.template;
 
-    debug(demoSrc, screenshotDest, template);
+        debug(demoSrc, screenshotDest, template);
 
-    const walker = walk(demoSrc, { followLinks: false });
-    walker.on('file', (root, stat, next) => {
-        const relativeName = relative(demoSrc, join(root, stat.name));
-        debug(`[file]: ${relativeName}`);
-        const ext = extname(stat.name);
-        if (ext === '.html' || ext === '.md') {
-            const htmlContent = renderFile(resolve(root, stat.name), template);
-            const datauri = new DataURI();
-            datauri.format('.html', htmlContent);
-            const fileBasename = relativeName
-                .replace(/\.html$/, '')
-                .replace(/\.md$/, '');
-            const outputFilename = join(screenshotDest, `${fileBasename}.png`);
-            q.defer(callback => {
-                const t0 = Date.now();
-                const nightmare = Nightmare({
-                    show: true
-                });
-                nightmare
-                    .viewport(800, 450)
-                    .goto(datauri.content)
-                    .wait('#mountNode canvas')
-                    .wait(DELAY)
-                    .click('#mountNode')
-                    .screenshot(outputFilename, () => {
-                        debug(`${fileBasename} toke ${Date.now() - t0} to take a screenshot.`);
-                        callback(null);
-                    })
-                    .end()
-                    .catch(e => {
-                        callback(e);
+        const walker = walk(demoSrc, { followLinks: false });
+        walker.on('file', (root, stat, next) => {
+            const relativeName = relative(demoSrc, join(root, stat.name));
+            debug(`[file]: ${relativeName}`);
+            if (relativeName === 'index.html') {
+                next();
+                return;
+            }
+            const ext = extname(stat.name);
+            if (ext === '.html' || ext === '.md') {
+                const htmlContent = renderFile(resolve(root, stat.name), template);
+                const fileBasename = relativeName
+                    .replace(/\.html$/, '')
+                    .replace(/\.md$/, '');
+                const targetFilename = join(screenshotDest, `${fileBasename}.html`);
+                writeFileSync(targetFilename, htmlContent, 'utf8');
+
+                const relativeUrl = relative(dest, targetFilename);
+                const targetUrl = join(url, relativeUrl);
+
+                const outputFilename = join(screenshotDest, `${fileBasename}.png`);
+                debug(`target: ${outputFilename}`);
+                q.defer(callback => {
+                    const t0 = Date.now();
+                    const nightmare = Nightmare({
+                        show: true
                     });
-            });
-        }
-        next();
-    });
-    walker.on('directory', (root, stat, next) => {
-        const relativeName = relative(demoSrc, join(root, stat.name));
-        debug(`[directory]: ${relativeName}`);
-        mkdir(resolve(screenshotDest, relativeName));
-        next();
-    });
-    walker.on('errors', (root, nodeStatsArray, next) => { // plural
-        nodeStatsArray.forEach(function (n) {
-            debug(`[ERROR] ${n.name}`);
-            debug(n.error.message || `${n.error.code}: ${n.error.path}`);
+                    nightmare
+                        .viewport(800, 450) // 16 x 9
+                        .goto(targetUrl)
+                        .wait('#mountNode canvas')
+                        .wait(DELAY)
+                        .screenshot(outputFilename, () => {
+                            debug(`${fileBasename} toke ${Date.now() - t0} to take a screenshot.`);
+                            callback(null);
+                        })
+                        .end()
+                        .catch(e => {
+                            callback(e);
+                        });
+                });
+            }
+            next();
         });
-        next();
-    });
-    walker.on('end', () => {
-        debug('stop walking');
+        walker.on('directory', (root, stat, next) => {
+            const relativeName = relative(demoSrc, join(root, stat.name));
+            debug(`[directory]: ${relativeName}`);
+            mkdir('-p', resolve(screenshotDest, relativeName));
+            next();
+        });
+        walker.on('errors', (root, nodeStatsArray, next) => { // plural
+            nodeStatsArray.forEach(function (n) {
+                debug(`[ERROR] ${n.name}`);
+                debug(n.error.message || `${n.error.code}: ${n.error.path}`);
+            });
+            next();
+        });
+        walker.on('end', () => {
+            debug('stop walking');
+            q.awaitAll(error => {
+                if (error) {
+                    debug(error);
+                    process.exit(1);
+                } else {
+                    debug('screenshots are all captured!');
+                    process.exit();
+                }
+            });
+        });
     });
 });
