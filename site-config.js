025789f8f7e68d7aa6491cdc619a97cf6b16360e
simaQ <sima.zhang1990@gmail.com>
2017-09-25 16:40:41 +0800

    g2 url

--- site-config.js
+++ site-config.js
@@ -6,7 +6,7 @@ const pkg = require('./package.json');
 module.exports = {
     url: {
         dataSet: 'https://unpkg.alipay.com/@alipay/data-set@0.4.1/build/data-set.js',
-        g2: 'https://gw.alipayobjects.com/os/rmsportal/cGsyueQNDXhNXPHALEjQ.js',
+        g2: 'https://unpkg.alipay.com/@alipay/g2@3.0.0-rc4.2/build/g2.js',
         jquery: '${assets}/lib/jquery-3.2.1.min.js',
     },
     assets: '/assets',
