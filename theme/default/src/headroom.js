224cf316690b21e29a24ccbd712a1320bb7f4089
leungwensen <leungwensen@gmail.com>
2017-10-10 14:21:40 +0800

    headroom standalone

---
+++ theme/default/src/headroom.js
@@ -0,0 +1,12 @@
+import './headroom.less';
+import $ from 'jquery';
+
+$('header').headroom({
+    offset: 205,
+    tolerance: 5,
+    classes: {
+        initial: 'animated',
+        pinned: 'slideDown',
+        unpinned: 'slideUp'
+    }
+});
--- theme/default/src/home.js
+++ theme/default/src/home.js
@@ -1,12 +1,2 @@
 import './home.less';
-import $ from 'jquery';
 
-$('header').headroom({
-    offset: 205,
-    tolerance: 5,
-    classes: {
-        initial: 'animated',
-        pinned: 'slideDown',
-        unpinned: 'slideUp'
-    }
-});
