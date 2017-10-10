c06a568af41074c0403ab9ba670836694db96036
leungwensen <leungwensen@gmail.com>
2017-10-10 14:14:16 +0800

    headroom stylesheets and animates, etc.

--- theme/default/src/home.js
+++ theme/default/src/home.js
@@ -1,1 +1,12 @@
 import './home.less';
+import $ from 'jquery';
+
+$('header').headroom({
+    offset: 205,
+    tolerance: 5,
+    classes: {
+        initial: 'animated',
+        pinned: 'slideDown',
+        unpinned: 'slideUp'
+    }
+});
