da5c02009c0d22e8f1d65da40eeba40eb842fba6
leungwensen <leungwensen@gmail.com>
2017-10-25 15:41:08 +0800

    tutorial for data-set, etc.

--- site-config.js
+++ site-config.js
@@ -8,11 +8,12 @@ var pkg = require('./package.json');
 module.exports = {
     url: {
         // TODO FIXME: use global CDN urls instead
-        dataSet: 'http://unpkg.alipay.com/@antv/data-set@0.5.2/build/data-set.js',
-        g2: 'http://unpkg.alipay.com/@antv/g2@3.0.0-rc5.2/build/g2.js',
-        g6: 'http://unpkg.alipay.com/@ali/g6@1.2.0-beta.32/dist/g6-all.js',
         'g6-plugins': 'http://unpkg.alipay.com/@ali/g6-plugins/dist/g6-plugins.min.js',
-        jquery: 'https://gw.alipayobjects.com/as/g/datavis/assets/1.0.1/jquery-3.2.1.min.js'
+        dataSet: 'http://unpkg.alipay.com/@antv/data-set@0.5.2/build/data-set.js',
+        g2: 'http://unpkg.alipay.com/@antv/g2@3.0.0-rc5.3/build/g2.js',
+        g6: 'http://unpkg.alipay.com/@ali/g6@1.2.0-beta.32/dist/g6-all.js',
+        jquery: 'https://gw.alipayobjects.com/as/g/datavis/assets/1.0.1/jquery-3.2.1.min.js',
+        lodash: 'https://gw.alipayobjects.com/as/g/datavis/assets/1.0.1/lodash-4.17.4.min.js',
     },
     assets: '/assets',
     base: '/',
