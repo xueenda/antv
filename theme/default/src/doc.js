afe3b6cddf33d52a5edb9e959bbe94a426c5310e
leungwensen <leungwensen@gmail.com>
2017-11-08 11:29:43 +0800

    update

--- theme/default/src/doc.js
+++ theme/default/src/doc.js
@@ -6,7 +6,7 @@ import './doc.less';
 tocbot.init({
     tocSelector: '.toc',
     contentSelector: '.doc-container .content',
-    headingSelector: 'h1, h2, h3, h4',
+    headingSelector: 'h1, h2',
     collapseDepth: 3,
 });
 
