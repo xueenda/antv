25021c4a6e2b92ae9593f7865bc93221de977877
jljsj <liangjun.jiang@alipay.com>
2017-11-16 14:35:57 +0800

    add home anim

---
+++ theme/default/src/Home/Banner.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import QueueAnim from 'rc-queue-anim';
+
+import BannerImage from './BannerImage';
+
+import { resource, products } from '../../../../site/zh-cn/data';
+
+import { assets } from './data';
+
+class Banner extends React.PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    isMobile: PropTypes.bool,
+  }
+  static defaultProps = {
+    className: 'banner',
+  }
+  render() {
+    const { className, isMobile } = this.props;
+    return (
+      <section className={`${className}-wrapper page`}>
+        <div className={className}>
+          <div className={`${className}-img-wrapper`}>
+            {isMobile ?
+              <img width="100%" src={`${assets}/image/home/intro-landscape.svg`} alt="" />
+              :
+              <BannerImage />}
+          </div>
+          <QueueAnim
+            type={isMobile ? 'bottom' : 'right'}
+            className={`${className}-text-wrapper`}
+            delay={300}
+          >
+            <h1 key="h1">让数据栩栩如生</h1>
+            <p className="main-info" key="p">
+              提供底层绘图引擎、具备完备图形语法大量产品实践，拥有专业可视 <br />
+              设计规范，蚂蚁数据可视化解决方案。
+            </p>
+            <a target="_blank" href={products.g2.href} key="a">
+              <button className="btn-round-link btn btn-primary btn-lg">
+                {resource.translate.getStarted}
+              </button>
+            </a>
+          </QueueAnim>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Banner;
---
+++ theme/default/src/Home/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import enquire from 'enquire.js';
+
+import Banner from './Banner';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
+import Page4 from './Page4';
+import Page5 from './Page5';
+import './home.index.less';
+
+function enquireScreen(cb) {
+  /* eslint-disable no-unused-expressions */
+  // and (min-width: 320px)
+  enquire.register('only screen and (max-width: 768px)', {
+    match: () => {
+      cb && cb(true);
+    },
+    unmatch: () => {
+      cb && cb();
+    },
+  });
+  /* eslint-enable no-unused-expressions */
+}
+
+let isMobile;
+enquireScreen((b) => {
+  isMobile = b;
+});
+
+
+class Home extends React.PureComponent {
+  state = {
+    isMobile,
+  };
+
+  componentDidMount() {
+    enquireScreen((b) => {
+      this.setState({
+        isMobile: !!b,
+      });
+    });
+  }
+  render() {
+    return (
+      [
+        <Banner key="banner" isMobile={this.state.isMobile} />,
+        <Page1 key="page1" />,
+        <Page2 key="page2" />,
+        <Page3 key="page3" />,
+        <Page4 key="page4" isMobile={this.state.isMobile} />,
+        <Page5 key="page5" />,
+      ]
+    );
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById('react-content'));
