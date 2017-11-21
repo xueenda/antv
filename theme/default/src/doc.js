07be620a9a084b9d3cfc4331357e860685c9c8ea
leungwensen <leungwensen@gmail.com>
2017-11-21 14:47:50 +0800

    doc: menu fixed

--- theme/default/src/doc.js
+++ theme/default/src/doc.js
@@ -11,6 +11,7 @@ tocbot.init({
 });
 
 const $tocContainer = $('.toc-container');
+const $menu = $('.menu.bd-sidebar');
 $('header').headroom({
     offset: 205,
     tolerance: 5,
@@ -24,11 +25,17 @@ $('header').headroom({
             top: '104px',
             height: 'calc(100% - 120px)',
         });
+        $menu.css({
+            'margin-top': 0
+        });
     },
     onUnpin() {
         $tocContainer.css({
             top: '32px',
             height: 'calc(100% - 48px)',
+        });
+        $menu.css({
+            'margin-top': '-64px'
         });
     }
 });
