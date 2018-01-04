e91d007151b536058932ece540477e11e0fb5d67
leungwensen <leungwensen@gmail.com>
2018-01-04 16:04:20 +0800

    fix(style): responsive details for demo page, blogs, etc.
    Closes: #1, #24

--- theme/default/src/demo.js
+++ theme/default/src/demo.js
@@ -45,6 +45,7 @@ const htmlEditor = CodeMirror.fromTextArea($code[0], {
 });
 
 const $iframeContainer = $('#preview');
+
 function syncCode() {
     $iframeContainer.html('<iframe></iframe>');
     const iframe = $iframeContainer.find('iframe')[0];
@@ -54,10 +55,21 @@ function syncCode() {
     iframeDoc.close();
 }
 
+$('header').headroom({
+    offset: 0,
+    tolerance: 0,
+    classes: {
+        initial: 'animated',
+        pinned: 'slideDown',
+        unpinned: 'slideUp'
+    }
+});
+
 function resizePreview() {
     $iframeContainer.height($iframeContainer.width() / 16 * 9);
     syncCode();
 }
+
 resizePreview();
 
 const $execute = $('#btn-execute');
