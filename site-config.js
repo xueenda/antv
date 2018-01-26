103fe893951843abc6a7732e7a297277eca110e5
leungwensen <leungwensen@gmail.com>
2018-01-27 00:35:10 +0800

    chore(demo): add demo for waffle chart

--- site-config.js
+++ site-config.js
@@ -6,9 +6,9 @@ var pkg = require('./package.json');
 
 module.exports = {
     url: {
-        dataSet: 'https://gw.alipayobjects.com/os/antv/assets/data-set/0.8.3/data-set.min.js',
+        dataSet: 'https://gw.alipayobjects.com/os/antv/assets/data-set/0.8.5/data-set.min.js',
         f2: 'https://gw.alipayobjects.com/os/antv/assets/f2/3.0.3/f2.js',
-        g2: 'https://gw.alipayobjects.com/os/antv/assets/g2/3.0.4-beta.2/g2.min.js',
+        g2: 'https://gw.alipayobjects.com/os/antv/assets/g2/3.0.4/g2.min.js',
         g6: 'https://gw.alipayobjects.com/os/antv/assets/g6/1.2.3/g6.min.js',
         'g6-plugins': 'https://gw.alipayobjects.com/os/antv/assets/g6-plugins/1.0.1/g6-plugins.min.js',
         'g2-plugin-slider': 'https://gw.alipayobjects.com/os/antv/assets/g2-plugin-slider/2.0.0/g2-plugin-slider.js',
