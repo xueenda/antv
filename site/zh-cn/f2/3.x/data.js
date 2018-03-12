1c8dc56e37dc79a9b8695b9a194299faf76471ca
sima.zhang1990@gmail.com <zaifei.zwx@alibaba-inc.com>
2018-03-12 18:35:02 +0800

    add f2 demos

--- site/zh-cn/f2/3.x/data.js
+++ site/zh-cn/f2/3.x/data.js
@@ -1,13 +1,11 @@
 const plotByName = {
-    point:  { index: 0,  icon: '', url: '#', name: '点图', },
-    line:   { index: 1,  icon: '', url: '#', name: '折线图', },
-    area:   { index: 2,  icon: '', url: '#', name: '面积图', },
-    bar:    { index: 3,  icon: '', url: '#', name: '柱状图', },
-    candle: { index: 4,  icon: '', url: '#', name: '烛形图', },
-    pie:    { index: 5,  icon: '', url: '#', name: '饼图', },
-    radar:  { index: 6, icon: '', url: '#', name: '雷达图', },
-    other:  { index: 10, icon: '', url: '#', name: '其他图表', },
-
+    line:   { index: 0,  icon: '', url: '#', name: '折线图', },
+    area:   { index: 1,  icon: '', url: '#', name: '面积图', },
+    bar:    { index: 2,  icon: '', url: '#', name: '柱状图', },
+    pie:    { index: 3,  icon: '', url: '#', name: '饼图', },
+    radar:  { index: 4, icon: '', url: '#', name: '雷达图', },
+    point:  { index: 5,  icon: '', url: '#', name: '点图', },
+    other:  { index: 6, icon: '', url: '#', name: '其他图表', },
 };
 module.exports = {
     plotByName,
