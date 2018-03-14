b0ec2514c1a1ac0d78db58b47bbeb604b9cd766d
sima.zhang1990@gmail.com <zaifei.zwx@alibaba-inc.com>
2018-03-14 17:16:30 +0800

    add chart descriptions for f2 demos

--- site/zh-cn/f2/3.x/data.js
+++ site/zh-cn/f2/3.x/data.js
@@ -1,11 +1,11 @@
 const plotByName = {
-    line:   { index: 0,  icon: '', url: '#', name: '折线图', },
-    area:   { index: 1,  icon: '', url: '#', name: '面积图', },
-    bar:    { index: 2,  icon: '', url: '#', name: '柱状图', },
-    pie:    { index: 3,  icon: '', url: '#', name: '饼图', },
-    radar:  { index: 4, icon: '', url: '#', name: '雷达图', },
-    point:  { index: 5,  icon: '', url: '#', name: '点图', },
-    other:  { index: 6, icon: '', url: '#', name: '其他图表', },
+    line:   { index: 0,  icon: 'icon-line', url: '${base}zh-cn/f2/3.x/demo/index.html#_折线图', name: '折线图', },
+    area:   { index: 1,  icon: 'icon-area', url: '${base}zh-cn/f2/3.x/demo/index.html#_面积图', name: '面积图', },
+    bar:    { index: 2,  icon: 'icon-bar', url: '${base}zh-cn/f2/3.x/demo/index.html#_柱状图', name: '柱状图', },
+    pie:    { index: 3,  icon: 'icon-pie', url: '${base}zh-cn/f2/3.x/demo/index.html#_饼图', name: '饼图', },
+    radar:  { index: 4, icon: 'icon-radar', url: '${base}zh-cn/f2/3.x/demo/index.html#_雷达图', name: '雷达图', },
+    point:  { index: 5,  icon: 'icon-point', url: '${base}zh-cn/f2/3.x/demo/index.html#_点图', name: '点图', },
+    other:  { index: 6, icon: 'icon-other', url: '${base}zh-cn/f2/3.x/demo/index.html#_其他图表', name: '其他图表', },
 };
 module.exports = {
     plotByName,
