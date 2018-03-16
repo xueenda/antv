ea9b01d379e358d244fd37e71f58ca602bd88e8a
leungwensen <leungwensen@gmail.com>
2018-03-17 01:24:54 +0800

    chore(gallery): add three tree layout demos

--- site-config.js
+++ site-config.js
@@ -7,7 +7,7 @@ var pkg = require('./package.json');
 module.exports = {
     url: {
         d3: 'https://gw.alipayobjects.com/os/antv/assets/lib/d3-4.13.0.min.js',
-        dataSet: 'https://gw.alipayobjects.com/os/antv/assets/data-set/0.8.5/data-set.min.js',
+        dataSet: 'https://gw.alipayobjects.com/os/antv/assets/data-set/0.8.6/data-set.min.js',
         f2: 'https://gw.alipayobjects.com/os/antv/assets/f2/3.0.3/f2.js',
         g2: 'https://gw.alipayobjects.com/os/antv/assets/g2/3.0.5-beta.5/g2.min.js',
         g6: 'https://gw.alipayobjects.com/os/antv/assets/g6/1.2.5/g6.min.js',
